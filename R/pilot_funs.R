cf8d01dd508a508b2e3cc5aa722875c3ad599a49
rsteele7 <rsteele7@vols.utk.edu>
2017-04-11 15:22:23 -0400

    fixed functions, time.col parameter working now

--- R/pilot_funs.R
+++ R/pilot_funs.R
@@ -2,18 +2,18 @@
 #'
 #' @param csv Filepath to and name of the csv file holding the data.
 #' @param OD.col Column holding optical density data.
-#' @param title The title of the plot
+#' @param time.col Column holding times at which data was collected; default is "Time"
+#' @param title The title of the plot; default is "Growth Curve"
 #' @param nm Wavelength in nanometers at which optical density was measured; default is 600
-#' @param time.col Column holding times at which data was collected
 #' @param time.unit Time unit in which data is collected; default is "min"
 #' @return Graph with single growth curve.
 #' @examples
 #' singraph("sample_data.csv", "OD600")
-#' singraph("sample_data.csv", "OD600", 300)
+#' singraph("sample_data.csv", "OD600", nm = 300)
 
-singraph <- function(csv, OD.col, title = "Growth Curve", nm = 600, time.col, time.unit = "min") {
+singraph <- function(csv, OD.col, time.col = "Time", title = "Growth Curve", nm = 600, time.unit = "min") {
   data <- read.csv(csv)
-  ggplot2::ggplot(data, aes(x=data$time.col, y=data$OD.col)) +
+  ggplot2::ggplot(data, aes(x=data[, time.col], y=data[, OD.col])) +
     geom_line(aes(y=data[, OD.col])) +
     geom_point(aes(y=data[, OD.col])) +
     scale_y_log10() +
@@ -27,10 +27,11 @@ singraph <- function(csv, OD.col, title = "Growth Curve", nm = 600, time.col, ti
 #'
 #' @param csv Filepath to and name of the csv file holding the data.
 #' @param OD.col Column holding optical density data.
+#' @param time.col Column holding times at which data was collected; default is "Time"
 #' @param midlog For the strain of interest, the typical midlog value.
 #' @return Generation doubling time in the same unit of time as in the data sheet.
 #' @examples
-#' singen("sample_data.csv", "OD600", 0.3)
+#' singen("sample_data.csv", "OD600", 0.3, time.col = "OD600)
 #' singen("sample_data.csv", "OD600", 0.4)
 
 singen <- function(csv, OD.col, midlog, time.col="Time") {
@@ -38,7 +39,7 @@ singen <- function(csv, OD.col, midlog, time.col="Time") {
   log_vals <- data[data[, OD.col] >= midlog, ]
   use_vals <- head(log_vals, 2)
   OD.diff <- log10(use_vals[2, OD.col]) - log10(use_vals[1, OD.col])
-  time.diff <- use_vals[2, "Time"] - use_vals[1, time.col]
+  time.diff <- use_vals[2, time.col] - use_vals[1, time.col]
   slope <- OD.diff/time.diff
   gen_time <- 0.3/slope
   return(gen_time)
--- R/trip_funs.R
+++ R/trip_funs.R
@@ -4,6 +4,7 @@
 #' @param OD_col1 Column holding first column of optical density data.
 #' @param OD_col2 Column holding second column of optical density data.
 #' @param OD_col3 Column holding third column of optical density data.
+#' @param time.col Column holding times at which data was collected; default is "Time"
 #' @param title Title of the plot
 #' @param nm Wavelength at which optical density was measured; default is 600nm
 #' @param time.unit Time unit in which data is collected; default is min
@@ -11,15 +12,15 @@
 #' tripgraph("sample_data_trip.csv", "OD600_1", "OD600_2", "OD600_3", title = "My growth curve in triplicate")
 #' tripgraph("sample_data_trip.csv", "OD600_1", "OD600_2", "OD600_3", nm = 200)
 
-tripgraph <- function(csv, OD_col1, OD_col2, OD_col3, title="Growth Curve", nm=600, time.unit = "min") {
+tripgraph <- function(csv, OD_col1, OD_col2, OD_col3, time.col = "Time", title="Growth Curve", nm=600, time.unit = "min") {
   data <- read.csv(csv)
   eval_cols <- c(OD_col1, OD_col2, OD_col3)
   data <- mutate(data, mean = rowMeans(data[, eval_cols]),
                  stdev = matrixStats::rowSds(as.matrix(data[, eval_cols])))
-  ggplot2::ggplot(data, aes(x=data$Time, y=mean)) +
+  ggplot2::ggplot(data, aes(x=data[, time.col], y=mean)) +
     geom_line(aes(y=mean)) +
     geom_point(aes(y=mean)) +
-    geom_errorbar(aes(ymin= mean- stdev, ymax = mean + stdev)) +
+    geom_errorbar(aes(ymin= mean - stdev, ymax = mean + stdev)) +
     scale_y_log10() +
     ylab(bquote("Log " ~ OD[.(nm)])) +
     xlab(bquote("Time " ~ (.(time.unit)))) +
@@ -35,19 +36,20 @@ tripgraph <- function(csv, OD_col1, OD_col2, OD_col3, title="Growth Curve", nm=6
 #' @param OD_col2 Column holding second column of optical density data.
 #' @param OD_col3 Column holding third column of optical density data.
 #' @param midlog For the strain of interest, the typical midlog value.
+#' @param time.col Column holding times at which data was collected; default is "Time"
 #' @return Generation doubling time in the same unit of time as in the data sheet.
 #' @examples
 #' tripgen("sample_data_trip.csv", "OD600_1", "OD600_2", "OD600_3", 0.3)
 #' tripgen("sample_data_trip.csv", "OD600_1", "OD600_2", "OD600_3", 0.3)
 
-tripgen <- function(csv, OD_col1, OD_col2, OD_col3, midlog) {
+tripgen <- function(csv, OD_col1, OD_col2, OD_col3, midlog, time.col = "Time") {
   data <- read.csv(csv)
   eval_cols <- c(OD_col1, OD_col2, OD_col3)
   data <- mutate(data, mean = rowMeans(data[, eval_cols]))
   log_vals <- data[data[, "mean"] >= midlog, ]
   use_vals <- head(log_vals, 2)
   OD_diff <- log10(use_vals[2, "mean"]) - log10(use_vals[1, "mean"])
-  time_diff <- use_vals[2, "Time"] - use_vals[1, "Time"]
+  time_diff <- use_vals[2, time.col] - use_vals[1, time.col]
   slope <- OD_diff/time_diff
   gen_time <- 0.3/slope
   return(gen_time)
