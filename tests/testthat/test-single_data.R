1f04cb5191bbf45b2b6d088910be8d63dc7fcd3b
rsteele7 <rsteele7@vols.utk.edu>
2017-05-01 10:46:00 -0400

    updated tests again

--- tests/testthat/test-single_data.R
+++ tests/testthat/test-single_data.R
@@ -1,4 +1,4 @@
 #-----------code to test singen() function-------------#
-test_that("singen('sample_data.csv', 'OD600', 0.3, time.col = 'Time')", {
-  expect_equal(singen("sample_data.csv", "OD600", 0.3, time.col = "Time"))
+test_that("singen('sample_data.csv', 'OD600', 0.3)", {
+  expect_equal(singen("sample_data.csv", "OD600", 0.3), 43.54039)
 })
--- tests/testthat/test-trip_data.R
+++ tests/testthat/test-trip_data.R
@@ -1,4 +1,4 @@
 #-----------code to test tripgen() function-------------#
-test_that("tripgen('sample_data.csv', 'OD600', 0.3, time.col = 'Time')", {
-  expect_equal(tripgen("sample_data.csv", "OD600", 0.3, time.col = "Time"))
+test_that("tripgen('sample_data_trip.csv', 'OD600_1', 'OD600_2', 'OD600_3', 0.3)", {
+  expect_equal(tripgen("sample_data_trip.csv", "OD600_1", "OD600_2", "OD600_3", 0.3), 43.8523)
 })
